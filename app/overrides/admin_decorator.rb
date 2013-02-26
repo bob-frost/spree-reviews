5301f58b096c1cad35025c458a670a8280164bdd
Maks Sleptsov <maksimator@gmail.com>
2013-02-26 17:06:36 +0200

    Admin menu items updated

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,14 +1,15 @@
 Deface::Override.new(
-    :virtual_path => "admin/shared/_product_sub_menu",
-    :name => "reviews_admin_product_sub_tabs",
-    :insert_bottom => "[data-hook='admin_product_sub_tabs']",
-    :text => "<%= tab(:reviews, :label => 'review_management') %>",
-    :disabled => false)
-
+    :virtual_path => 'spree/admin/shared/_product_sub_menu',
+    :name => 'reviews_admin_product_sub_tabs',
+    :insert_bottom => '[data-hook="admin_product_sub_tabs"]',
+    :text => %q{<%= tab(:reviews, :label => 'review_management') %>},
+    :disabled => false
+)
 
 Deface::Override.new(
-    :virtual_path => "admin/configurations/index",
-    :name => "reviews_admin_configurations_menu",
-    :insert_bottom => "[data-hook='admin_configurations_menu']",
-    :text => "<%= configurations_menu_item(t('spree_reviews.review_settings'), admin_review_settings_path, t('spree_reviews.manage_review_settings')) %>",
-    :disabled => false)
+    :virtual_path => 'spree/admin/shared/_configuration_menu',
+    :name => 'add_social_providers_link_configuration_menu',
+    :insert_bottom => %q{[data-hook='admin_configurations_sidebar_menu']},
+    :text => %q{<%= configurations_sidebar_menu_item(t('spree_reviews.review_settings'), admin_review_settings_path) %>},
+    :disabled => false
+)
