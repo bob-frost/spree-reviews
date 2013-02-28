398c34212d07f5b85b8e67fc5ecf3e2f8da01af9
Maks Sleptsov <maksimator@gmail.com>
2013-02-28 13:40:47 +0200

    Feedbacks management page layout updated

--- app/controllers/spree/admin/feedback_reviews_controller.rb
+++ app/controllers/spree/admin/feedback_reviews_controller.rb
@@ -1,28 +1,8 @@
 class Spree::Admin::FeedbackReviewsController < Spree::Admin::ResourceController
 
-  # TODO: update layout to fit 1.3 conventions
-
   def index
     @review = Spree::Review.find(params[:review_id])
     @collection = @review.feedback_reviews
-  end
-
-  def create
-    respond_to do |format|
-      format.html { redirect_to collection_path }
-    end
-  end
-
-  def update
-    respond_to do |format|
-      format.html { redirect_to collection_path }
-    end
-  end
-
-  def destroy
-    respond_to do |format|
-      format.html { redirect_to collection_path }
-    end
   end
 
 end
